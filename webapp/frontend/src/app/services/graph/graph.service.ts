0cf6796703f95d657d1f0d6e876882593cf09ecc
Arunas Vaitkus <arunas21@gmail.com>
2017-02-10 15:44:17 +0100

    add nodeexpand

--- webapp/frontend/src/app/services/graph/graph.service.ts
+++ webapp/frontend/src/app/services/graph/graph.service.ts
@@ -9,6 +9,8 @@ private settings =  {
 endpoint: 'http://localhost:5050/graph/query/id/'
 };
 private dataSource = new Subject<any>();
+private REFERENCED_NODES :string ="referenced-nodes/";
+
 // Observable string streams
 dataSourced$ = this.dataSource.asObservable();
 
@@ -89,4 +91,57 @@ constructor(
         return output
       })
   }
+
+  //localhost:5050/graph/query/id/referenced-nodes/9112
+  getOutboundNodes ( id: number , graph: any): Observable<any> {
+    return this.http.get (this.settings.endpoint + this.REFERENCED_NODES + id)
+      .map (res => {
+        let results = res.json().network;
+
+        for (var i = 0; i < results.nodes.length; i++) {
+
+          var checkExists: boolean = false;
+          var currentSize: number = graph.nodes.length;
+          for (var k = 0; k < currentSize; k++) {
+            if (results.nodes[i].id === graph.nodes[k].id) {
+              checkExists = true;
+            }
+          }
+          if (checkExists != true) {
+            graph.nodes.push({
+              id: results.nodes[i].id,
+              name: results.nodes[i].label,
+              group: results.nodes[i].types[0], //todo: fix this and use more groups
+              img: (results.nodes[i].depiction == null) ? null : results.nodes[i].depiction,
+              description: (results.nodes[i].description == null) ? null : results.nodes[i].description,
+              url: results.nodes[i].swapi_url
+            });
+          }
+        }
+
+       for (var i = 0; i < results.edges.length; i++) {
+
+          var checkExists: boolean = false;
+          var currentSize: number = graph.links.length;
+          for (var k = 0; k < currentSize; k++) {
+            if (results.edges[i].from === graph.links[k].source && results.edges[i].to === graph.links[k].target ) {
+              checkExists = true;
+              console.log("duplicate link! ");
+            }
+          }
+           if (checkExists != true) {
+          graph.links.push({
+            source: results.edges[i].from,
+            target: results.edges[i].to,
+            strength: 1,
+            arrows: results.edges[i].arrows[1],
+            relationship: results.edges[i].label
+          });
+          }
+        }
+        return graph;
+      })
+      .catch ( (error:any) => Observable.throw (error.json() || 'Server error'));
+  }
+
 }
--- webapp/frontend/src/app/shared/force/force.component.ts
+++ webapp/frontend/src/app/shared/force/force.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, OnChanges, ViewChild, ElementRef, Input, ViewEncapsulation} from '@angular/core';
 import * as d3 from 'd3';
+import { GraphService } from '../../services/graph/graph.service';
 
 @Component({
   selector: 'app-force',
@@ -9,30 +10,33 @@ import * as d3 from 'd3';
   ],
   encapsulation: ViewEncapsulation.None
 })
+
 export class ForceComponent implements OnInit {
   @ViewChild('chart') private chartContainer: ElementRef;
   @Input() private data: Array<any>;
-  private margin: any = { top: 20, bottom: 20, left: 20, right: 20 };
+  private margin: any = {top: 20, bottom: 20, left: 20, right: 20};
   private chart: any;
   private width: number;
   private height: number;
-  private nodeRadius: number = 25;
+  private initialData: any;
 
 
-  constructor() {
+  constructor(
+    private graph: GraphService
+  ) {
   }
 
 
   ngOnInit() {
 
-    this.createChart ();
+    this.createChart();
     if (this.data) {
-      this.update (this.data);
+      this.updateChart(this.data);
     }
   }
 
   ngOnChanges() {
-    this.update(this.data)
+    this.updateChart(this.data)
   }
 
   createChart() {
@@ -41,216 +45,249 @@ export class ForceComponent implements OnInit {
     let element = this.chartContainer.nativeElement;
     let width = element.offsetWidth - this.margin.left - this.margin.right;
     let height = element.offsetHeight - this.margin.top - this.margin.bottom;
-    let svg =  d3.select(element).append("svg")
+    let svg = d3.select(element).append("svg")
         .attr("id", "force")
         .attr("width", element.offsetWidth)
         .attr("height", element.offsetHeight)
       ;
   }
 
-  update(data) {
+  updateChart(data) {
     let element = this.chartContainer.nativeElement;
     let width = element.offsetWidth - this.margin.left - this.margin.right;
     let height = element.offsetHeight - this.margin.top - this.margin.bottom;
 
-    d3.select("#chart").remove();
+    var margin: any = this.margin;
+    var linkLabel: any;
+    var linkboundBox: any;
+    var links: any;
+    var link: any;
+    var simulation: any;
+    var node: any;
+    var defs: any;
+    var container: any;
+    var nodeRadius: number = 25;
+    var graphF: any = this.graph;
 
-    //chart plot
-	let svg = d3.select("svg")
-    this.chart = svg.append("g")
-      .attr("id", "chart")
-      .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`)
-      .call(d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended)
-)
+    clear();
+    build(data);
+
+
+    function clear() {
+      return d3.select("#chart").remove()
+    }
+
+    function build(data) {
+      //chart plot
+      let svg = d3.select("svg")
+      container = svg.append("g")
+        .attr("id", "chart")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)
+        .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended)
+        )
 
 // .on("keypress", keypress)
-;
+      ;
 
-/*
-Define <defs> part of svg. This is going to contain any reusable definitions of shapes
-*/
+      /*
+       Define <defs> part of svg. This is going to contain any reusable definitions of shapes
+       */
 
-var defs = this.chart.append("defs")
-
-var clipart = defs.append("clipPath")
-.attr("id", "circle-view")
-      .append("circle")
-      .style("fill", "#555")
-      .style("stroke-width", "4")
-      .style("stroke", "#455")
-      .attr("r", this.nodeRadius - 1);
+      defs = container.append("defs")
 
 
-    var arrowheads = defs.selectAll("marker").data(["end"])
-      .enter().append("svg:marker")
-      .attr("id", String)
-      .attr("viewBox", "0 -5 10 10")
-      //refX value must be the same as radius of an outer circle - this way it will be pointing correctly
-      .attr("refX", 37)
-      .attr("refY", 0)
-      .attr("markerWidth", 9)
-      .attr("markerHeight", 9)
-      .attr("orient", "auto")
-      .style('fill', "#aaa")
-      .append("svg:path")
-      .attr("d", "M0,-5L10,0L0,5");
+
+      defs.selectAll("marker").data(["end"])
+        .enter().append("svg:marker")
+        .attr("id", String)
+        .attr("viewBox", "0 -5 10 10")
+        //refX value must be the same as radius of an outer circle - this way it will be pointing correctly
+        .attr("refX", 37)
+        .attr("refY", 0)
+        .attr("markerWidth", 9)
+        .attr("markerHeight", 9)
+        .attr("orient", "auto")
+        .style('fill', "#aaa")
+        .append("svg:path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+      defs.append("clipPath")
+        .attr("id", "circle-view")
+        .append("circle")
+        .style("fill", "#555")
+        .style("stroke-width", "4")
+        .style("stroke", "#455")
+        .attr("r", nodeRadius - 1);
+
+      var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+      //group all links under one <g class="links">
+      var linksGrp = container
+          .append("g")
+          .attr("class", "links")
+        ;
+      var linkGrp = linksGrp
+          .selectAll("line")
+          .data(data.links)
+        ;
+      link = linkGrp
+        .enter()
+        .append("g")
+      ;
 
 
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-    var simulation = d3.forceSimulation()
-        .velocityDecay (0.2)
-      .force("charge", d3.forceManyBody().strength(-1000).distanceMax(400).distanceMin(1))
-      .force("center", d3.forceCenter(width / 2, height / 2));
+      linkGrp.selectAll("line").exit().remove();
 
 
-    //group all links under one <g class="links">
-    var link = this.chart.append("g")
-      .attr("class", "links")
-      .selectAll("line")
-      .data(data.links)
-      .enter().append("g");
+      links = link
+        .append("line")
+        .attr("marker-end", "url(#end)")
+      ;
 
 
-    //group all nodes under one <g class="nodes"
-    var node = this.chart.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(data.nodes)
+      linkLabel = link.append("text")
+        .attr("class", "linkLabel")
+        .attr("font-size", "8px")
+        .attr("x", -20)
+        .attr("dy", ".35em")
+        // .attr("filter", "url(#solid)")
+        .text(function (d: any) {
+          return d.relationship;
+        }).call(getTextBox);
+
+      //https://bl.ocks.org/mbostock/1160929
+      linkboundBox = link.insert("rect", "text")
+        .attr("x", function (d: any) {
+          return -20
+        })
+        .attr("y", function (d: any) {
+          return -2
+        })
+        .attr("width", function (d: any) {
+          return d.bbox.width + 1
+        })
+        .attr("height", function (d: any) {
+          return d.bbox.height + 3
+        })
+        .style("fill", "#fff");
+
+      //group all nodes under one <g class="nodes"
+      var nodesGrp = container.append("g")
+        .attr("class", "nodes");
+      var nodeGroup = nodesGrp.selectAll("circle").data(data.nodes);
+
+      node = nodeGroup
         .enter().append("g")
         .attr("class", "node")
-        // .on ("dblclick", nodeDoubleClicked)
+        .on("dblclick", nodeDoubleClicked)
         // .on ("contextmenu", contextMenu)
         .call(d3.drag()
           .on("start", dragstarted)
           .on("drag", dragged)
           .on("end", dragended)
-)
-;
+        )
+      ;
+
+      nodeGroup.selectAll("circle").exit().remove();
 
 
-var links = link.append("line")
-.attr("marker-end", "url(#end)");
 
 
-    var linkLabel = link.append("text")
-      .attr("class", "linkLabel")
-      .attr("font-size", "8px")
-      .attr("x", -20)
-      .attr("dy", ".35em")
-      // .attr("filter", "url(#solid)")
-      .text(function (d: any) {
-        return d.relationship;
-      }).call(getTextBox);
+      // Define the div for the tooltip
+      var div = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
-    //https://bl.ocks.org/mbostock/1160929
-    var linkboundBox = link.insert("rect", "text")
-      .attr("x", function (d: any) {
-        return -20
-      })
-      .attr("y", function (d: any) {
-        return -2
-      })
-      .attr("width", function (d: any) {
-        return d.bbox.width + 1
-      })
-      .attr("height", function (d: any) {
-        return d.bbox.height + 3
-      })
-      .style("fill", "#fff");
 
+      //circle with a opaque fill so that lines in background are not visible
+      var circle = node.append("circle")
+        .attr("r", nodeRadius)
+        .style("fill", "#eee");
+
+      var circle = node.append("circle")
+        .attr("r", nodeRadius)
+        //change the outer layer of circle's colour
+        .style("stroke", function (d: any, i: any) {
+          return color(d.group);
+        })
+        //.style("fill", "#eee")
+        .style("fill", function (d: any, i: any) {
+          return color(d.group);
+        })
+        .style("fill-opacity", 0.2);
+
+      var text = node.append("text")
+        .text(function (d: any) {
+          return d.name;
+        })
+        .attr("class", "node-text")
+        .attr("text-anchor", "middle")
+        .attr("dy", ".35em")
+        .attr("clip-path", "url(#circle-view)");
+
+      var image = node.append("image")
+        // width height directly relates to inverse x/y here. divide by 2 here
+          .attr("x", function (d) {
+            return -40;
+          })
+          .attr("y", function (d) {
+            return -40;
+          })
+          .attr("height", 80)
+          .attr("width", 80)
+          .attr("xlink:href", function (d: any) {
+            return d.img
+          })
+          .attr("clip-path", "url(#circle-view)")
+          .on("mouseover", function (d: any) {
+            div.transition()
+              .delay(300)
+              .duration(200)
+              .style("opacity", .9);
+            div.html(getTooltipText(d))
+              .style("left", (d3.event.pageX) + "px")
+              .style("top", (d3.event.pageY - 28) + "px");
+          })
+          .on("mouseout", function (d: any) {
+            div.transition()
+              .duration(500)
+              .style("opacity", 0);
+          })
+          .on("mousedown", function (d: any) {
+            div.transition()
+              .duration(100)
+              .style("opacity", 0);
+          })
+        ;
+      simulate()
+    }
+
+    function simulate() {
+
+      simulation = d3.forceSimulation()
+        .velocityDecay(0.2)
+        .force("charge", d3.forceManyBody().strength(-1000).distanceMax(400).distanceMin(1))
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+      simulation
+        .nodes(data.nodes)
+        .on("tick", ticked);
+
+      simulation.force("link", d3.forceLink().distance(50).strength(0.8).id(function (d: any) {
+        return d.id;
+      }).links(data.links))
+
+    }
 
     function getTextBox(selection) {
       selection.each(function (d) {
         d.bbox = this.getBBox();
       })
     }
-
-
-    // Define the div for the tooltip
-    var div = d3.select("body")
-      .append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 0);
-
-
-    //circle with a opaque fill so that lines in background are not visible
-    var circle = node.append("circle")
-      .attr("r", this.nodeRadius)
-      .style("fill", "#eee");
-
-    var circle = node.append("circle")
-      .attr("r", this.nodeRadius)
-      //change the outer layer of circle's colour
-      .style("stroke", function (d: any, i: any) {
-        return color(d.group);
-      })
-      //.style("fill", "#eee")
-      .style("fill", function (d: any, i: any) {
-        return color(d.group);
-      })
-      .style("fill-opacity", 0.2);
-
-    var text = node.append("text")
-      .text(function (d: any) {
-        return d.name;
-      })
-      .attr("class", "node-text")
-      .attr("text-anchor", "middle")
-      .attr("dy", ".35em")
-      .attr("clip-path", "url(#circle-view)");
-
-    var image = node.append("image")
-      // width height directly relates to inverse x/y here. divide by 2 here
-        .attr("x", function (d) {
-          return -40;
-        })
-        .attr("y", function (d) {
-          return -40;
-        })
-        .attr("height", 80)
-        .attr("width", 80)
-        .attr("xlink:href", function (d: any) {
-          return d.img
-        })
-        .attr("clip-path", "url(#circle-view)")
-        .on("mouseover", function (d: any) {
-          div.transition()
-            .delay(300)
-            .duration(200)
-            .style("opacity", .9);
-          div.html(getTooltipText(d))
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function (d: any) {
-          div.transition()
-            .duration(500)
-            .style("opacity", 0);
-        })
-        .on("mousedown", function (d: any) {
-          div.transition()
-            .duration(100)
-            .style("opacity", 0);
-        })
-      ;
-
-    /* uncomment this if tooltip does not look good
-     node.append("title")
-     .text(function(d: any) { return d.id; });
-     */
-
-
-    simulation
-      .nodes(data.nodes)
-      .on("tick", ticked);
-
-    simulation.force("link", d3.forceLink().distance (50).strength (0.8).id(function (d: any) {
-      return d.id;
-    }).links(data.links))
 
     function getTooltipText(data) {
       let description = '<h2 class="tooltip-name">' + data.group + ": " + data.name + '</h2>';
@@ -268,7 +305,7 @@ var links = link.append("line")
 
     function ticked() {
       // TODO: may want to combine link label with white box underneath so less calculations are done. may have performance issues later on
-      linkLabel.attr("transform", function (d: any) {
+     linkLabel.attr("transform", function (d: any) {
         var angle = Math.atan((d.source.y - d.target.y) / (d.source.x - d.target.x)) * 180 / Math.PI;
         return 'translate(' + [((d.source.x + d.target.x) / 2 ), ((d.source.y + d.target.y) / 2 )] + ')rotate(' + angle + ')';
       });
@@ -320,8 +357,16 @@ var links = link.append("line")
       if (!d3.event.active) simulation.alphaTarget(0);
     }
 
-    function nodeDoubleClicked(d) {
-      // expandOutbound(d.name);
+    function nodeDoubleClicked(d: any) {
+      let network: any = data;
+      setTimeout(() => {
+        graphF.getOutboundNodes(d.id, network).subscribe(data => {
+          clear();
+          build(data);
+        });
+      }, 4000);
+
+
     }
 
     function contextMenu(d) {
@@ -351,27 +396,6 @@ var links = link.append("line")
     // }
 
 
-  }
-
-
-
-  // TODO: Work in progress
-  updateChart (data) {
-
-    let updateLinks = this.chart.selectAll ("line")
-      .data (data.links);
-
-    // remove  links
-    updateLinks.exit().remove();
-
-    // update existing circles
-    let updateNodes = this.chart.selectAll ("circle")
-      .data (data.nodes);
-
-    // add new nodes
-    updateNodes
-      .enter ().append ("g")
-      .attr ("class", "node");
   }
 
 
